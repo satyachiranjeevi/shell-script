fe164ac2c3ac9036afea23e52874a58224bb2806
satyachiranjeevi <duvvu.chiranjeevi@gmail.com>
2023-08-17 17:56:29 +0100

    updated 05

--- 08.install-nginx.sh
+++ 08.install-nginx.sh
@@ -7,9 +7,17 @@ USERID=$(id -u)
 if [ $USERID -ne 0]
 then
     echo "ERROR:: pleae run the script with root access"
-else
-    echo "INFO:: You are root user"
+    esit 1
+#else
+#   echo "INFO:: You are root user"
 fi
 
 yum install nginx -y
 
+if [ $? -ne 0 ]
+then
+    echo "Error installing nginx"
+else
+    systemctl enable nginx
+    systemctl start nginx
+fi
