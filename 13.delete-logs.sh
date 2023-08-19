bd0275897a69f26c3b6f6f3167b7219085c4d068
satyachiranjeevi <duvvu.chiranjeevi@gmail.com>
2023-08-19 16:51:12 +0100

    second commit

--- 13.delete-logs.sh
+++ 13.delete-logs.sh
@@ -3,15 +3,19 @@
 
 APP_LOGS_DIR=/home/centos/app-logs
 
-DATE=$(date +%F)
-LOGSDIR=/home/centos/shell-script-logs
+DATE=$(date +%F:%H:%M:%S)
+LOGSDIR=/home/centos/shellscript-logs
 
 SCRIPT_NAME=$0
 LOGFILE=$LOGSDIR/$0-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
+echo "Script started"
+
 echo "$FILES_TO_DELETE"
+
+echo "Script Ended"
 
 
 while read line
