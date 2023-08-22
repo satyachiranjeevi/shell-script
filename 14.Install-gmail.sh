0483de856d8c3e08323d2b01f0e6d25d7832de82
satyachiranjeevi <duvvu.chiranjeevi@gmail.com>
2023-08-22 23:35:44 +0100

    14. Gmail installation

--- 14.Install-gmail.sh
+++ 14.Install-gmail.sh
@@ -79,7 +79,7 @@ echo "PASSWORD is $PASSWORD"
 
 cp mail-cred /etc/postfix/sasl_passwd
 
-sed -i "s/UNAME/duvvu.chiranjeevi@gmail.com" -i "s/PASSWORD/tldlvpkupgrmoxqt" /etc/postfix/sasl_passwd
+#sed -i "s/UNAME/duvvu.chiranjeevi@gmail.com" -i "s/PASSWORD/tldlvpkupgrmoxqt" /etc/postfix/sasl_passwd
 
 postmap /etc/postfix/sasl_passwd 
 
