ec11d009ecb3e9bbad58e9a878fc6824c27b998d
satyachiranjeevi <duvvu.chiranjeevi@gmail.com>
2023-08-22 21:56:38 +0100

    14. Gmail installation

---
+++ disk-usage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+LOGFILE_DIRECTORY=/tmp
+DATE=$(date +%F:%H:%M:%S)
+SCRIPT_NAME=$0
+LOGFILE=$LOGFILE_DIRECTORY/$SCRIPT_NAME-$DATE.log
+
+
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+DISK_USAGE=$(df -hT | grep -vE 'tmpfs|Filesystem')
+
+DISK_USAGE_THRESHOLD=1
+
+#IFS= means Internal Field Separator
+
+while IFS= read Line
+do
+    echo "output: $line"
+done <<< $DISK_USAGE
